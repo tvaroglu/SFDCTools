4ebb6ac5d849ad1eddf0687310fef2f09e1c4939
Taylor Varoglu <tava8398@colorado.edu>
2021-11-02 22:11:35 -0600

    Change directory naming for data_archive module

--- DataArchive/DataArchive.bat
+++
@@ -1,2 +0,0 @@
-"C:\Users\tvaroglu\AppData\Local\Programs\Python\Python38-32\python.exe" "C:\Users\tvaroglu\Desktop\Product Dev\Coding\Testing\SFDCTools\DataArchive\DataArchive.py"
-pause
---
+++ data_archive/data_archive.bat
@@ -0,0 +1,2 @@
+"C:\Users\tvaroglu\AppData\Local\Programs\Python\Python38-32\python.exe" "C:\Users\tvaroglu\Desktop\Product Dev\Coding\Testing\SFDCTools\data_archive\data_archive.py"
+pause
